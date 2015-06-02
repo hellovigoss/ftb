b180abab66627c38ad675b79bcd8e1ce40dd278b
hellovigoss <hellovigoss@gmail.com>
2015-06-02 20:21:24 +0800

    init

---
+++ ftb.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+
+//参数获取
+var arguments = process.argv.splice(2);
+if(arguments.length < 1){
+	console.log('Usage: node ftb.js path');
+}
+//转换目录
+var dirPath = arguments[0];
+//系统所有转换结果
+var funcArr = [];
+
+//遍历文件
+fileList = [];
+function walk(path){
+	var dirList = fs.readdirSync(path);
+
+	dirList.forEach(function(item){
+		if(fs.statSync(path + '/' + item).isFile()){
+			fileList.push(path + '/' + item);
+		}
+	});
+
+	dirList.forEach(function(item){
+		if(fs.statSync(path + '/' + item).isDirectory()){
+			walk(path + '/' + item);
+		}
+	});
+}
+
+walk(dirPath);
+
+//分析ftb元素
+fileList.forEach(function(filePath){
+	analysis(filePath);
+});
+
+//拼接js代码
+var ajaxJS = ";";
+funcArr.forEach(function(func){
+	var ajaxArgs = [];
+	func.args.forEach(function(arg){
+		ajaxArgs.push("\""+arg+"\":"+arg);
+	});
+	ajaxJS += "function "+func.name+"(";
+	ajaxJS += (func.args).join(",");
+	ajaxJS += "){$.ajax({url:'',type:'',data:{"+ajaxArgs.join(",")+"}})}";
+});
+
+//写入文件
+console.log(ajaxJS);
+
+function analysis(filePath){
+	var data = fs.readFileSync(filePath, "utf-8");
+
+	//php解析开始
+	//find ftb
+	var funcRegex = /\/\/\@ftb *(.*?)\W*?(\/[\W\w]*?\/)?\W*?function *(\w*?)\(.*\)/g;
+	var funcMatch;
+
+	//find function name and args
+	while(funcMatch =  funcRegex.exec(data)){
+		var funcName;
+		if(funcMatch[1] != ''){
+			funcName = funcMatch[1];
+		}
+		else{
+			funcName = funcMatch[3];
+		}
+		var argRegex = /\@params?\W*(\w*).*ftb/g;
+		var argMatch = '', funcArgv = [];
+		while(argMatch = argRegex.exec(funcMatch[2])){
+			funcArgv.push(argMatch[1]);
+		}
+		funcArr.push({
+			name: funcName,
+			args: funcArgv
+		});
+	}
+}
---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+class Test extends Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        
+    }
+	//@ftb asdf
+	/**
+	 * @params a ftb
+	 * @params b asdf asdf  ftb
+	 */
+    function action(){
+    }
+	//@ftb
+    function anotherAction(){
+    }
+}
